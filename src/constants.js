c0a02de8fc8c490d51208c755e1dafd51ed27ea3
Dragonswho <dragonswho@gmail.com>
2025-12-08 19:27:57 +0300

    chore(release): v1.0.2

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@
 
 export const APP_NAME = "Cyoa Brew";
 export const APP_ID = "cyoa_brew"; // Технический ID для проверок
-export const APP_VERSION = "1.0"; // Текущая версия
+export const APP_VERSION = "1.0.2"; // Текущая версия
 
 // Версия, ниже которой мы считаем сейвы "древними" и опасными
 export const MIN_COMPATIBLE_VERSION = "1.0"; 
