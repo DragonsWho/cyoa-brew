afa72e91c296983ded2d0154c2df5aa92d7a5e38
Dragonswho <dragonswho@gmail.com>
2025-12-14 11:03:38 +0300

    1.2.4

--- src/constants.js
+++ src/constants.js
@@ -4,7 +4,7 @@
 
 export const APP_NAME = "Cyoa Brew";
 export const APP_ID = "cyoa_brew"; // Технический ID для проверок
-export const APP_VERSION = "1.1.4"; // Текущая версия
+export const APP_VERSION = "1.2.4"; // Текущая версия
 
 // Версия, ниже которой мы считаем сейвы "древними" и опасными
 export const MIN_COMPATIBLE_VERSION = "1.0"; 
